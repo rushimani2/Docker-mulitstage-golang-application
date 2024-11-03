71ca30e2f7e7a2bd6bbace46e19bf28dd066f7c9
root <root@ip-172-31-27-217.ec2.internal>
2024-11-03 18:17:54 +0000

    third commit

--- second/add.go
+++ second/add.go
@@ -1,53 +1,62 @@
 package main
- 
- import (
-         "fmt"
-         "time"     
- )
- 
- func main() {       
-    currentTime := time.Now()        
-    fmt.Println("\n######################################\n")
-    fmt.Println(currentTime.Format("2006-01-02 15:04:05"))
-     
-    fmt.Println("\n######################################\n")   
-    timeStampString := currentTime.Format("2006-01-02 15:04:05")    
-    layOut := "2006-01-02 15:04:05"     
-    timeStamp, err := time.Parse(layOut, timeStampString)
-    if err != nil {
-        fmt.Println(err)          
-    }   
-    hr, min, sec := timeStamp.Clock()
-     
-    fmt.Println("Year   :", currentTime.Year())
-    fmt.Println("Month  :", currentTime.Month())
-    fmt.Println("Day    :", currentTime.Day())
-    fmt.Println("Hour   :", hr)
-    fmt.Println("Min    :", min)
-    fmt.Println("Sec    :", sec)    
-     
-    fmt.Println("\n######################################\n")   
-    year, month, day := time.Now().Date()
-    fmt.Println("Year   :", year)
-    fmt.Println("Month  :", month)
-    fmt.Println("Day    :", day)
-     
-    fmt.Println("\n######################################\n")          
-    t := time.Now()
-     
-    y := t.Year()
-    mon := t.Month()
-    d := t.Day()
-    h := t.Hour()
-    m := t.Minute()
-    s := t.Second()
-    n := t.Nanosecond()
-     
-    fmt.Println("Year   :",y)
-    fmt.Println("Month   :",mon)
-    fmt.Println("Day   :",d)
-    fmt.Println("Hour   :",h)
-    fmt.Println("Minute :",m)
-    fmt.Println("Second :",s)
-    fmt.Println("Nanosec:",n)
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+	"os/exec"
+)
+
+const (
+	width    = 40
+	height   = 10
+	numStars = 20
+)
+
+func clearScreen() {
+	cmd := exec.Command("clear") // For Linux/Unix
+	if err := cmd.Run(); err != nil {
+		cmd = exec.Command("cmd", "/c", "cls") // For Windows
+		cmd.Run()
+	}
 }
+
+func generateStars() [numStars][2]int {
+	stars := [numStars][2]int{}
+	for i := 0; i < numStars; i++ {
+		stars[i][0] = rand.Intn(width)
+		stars[i][1] = rand.Intn(height)
+	}
+	return stars
+}
+
+func printStars(stars [numStars][2]int) {
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			found := false
+			for _, star := range stars {
+				if star[0] == x && star[1] == y {
+					fmt.Print("*")
+					found = true
+					break
+				}
+			}
+			if !found {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println()
+	}
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	for {
+		clearScreen()
+		stars := generateStars()
+		printStars(stars)
+		time.Sleep(200 * time.Millisecond) // Adjust speed here
+	}
+}
+
